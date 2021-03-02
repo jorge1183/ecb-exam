dae1c4a4f63ae476c5cd1661c164bba5f9bf2d71
jorge1183 <henrygonz83@gmail.com>
2021-03-02 16:17:43 -0500

    Add files via upload

---
+++ exam/src/App.js
@@ -0,0 +1,152 @@
+import logo from './logo.svg';
+import './App.css';
+import { Col, Image, ListGroup, Row, Button, Modal, ButtonGroup } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+
+function App() {
+
+  const [carList, setCarList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [editRecord, setEditRecord] = useState({});
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  function fetchData(){
+    fetch('api/cars')
+    .then(r => r.json())
+    .then(j => setCarList(j));
+  }
+
+  function saveRecord(car) {
+    setLoading(true);
+    fetch(`api/cars?id=${car.id}`, {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(car)})
+      .then(() => {
+        fetchData();   
+      },() => {
+        alert('An error ocurred during update');
+      }).finally(() => {
+        setLoading(false);
+      });
+  }
+
+  function handleNotMaintenanceClick(e, i) {
+    e.stopPropagation();
+    const newArr = [...carList];
+    newArr[i].inMaintenance = false;
+    saveRecord(newArr[i]);
+  }
+
+  function handleEditRecordClick(e, i) {
+    e.stopPropagation();
+    setEditRecord({...carList[i]});
+    setShowModal(true);
+  }
+
+  function handleClose() {
+    setShowModal(false);
+  }
+
+  function handleChange(e) {
+    const newRecord = {...editRecord};
+    newRecord[e.target.name] = e.target.value;
+    setEditRecord(newRecord);
+  }
+
+  function handleSaveClick() {
+    editRecord.inMaintenance = true;
+    saveRecord(editRecord);
+    setShowModal(false);
+  }
+
+  return (
+    <div>
+      <h5>Click on a record to edit</h5>
+      <ListGroup>
+        {
+          carList.map((c, i) => 
+            <ListGroup.Item 
+              key={i} 
+              disabled={loading} 
+              className={c.inMaintenance === true ? 'in-maintenance' : ''}
+              onClick={(e) => handleEditRecordClick(e, i)}>
+              <Row>
+                <Col xs={2}>
+                  <Image className={"car-image"} src={c.image}/>
+                </Col>
+                <Col xs={10} >
+                  <Row>
+                    <Col xs={1}>
+                      Id: {c.id}
+                    </Col>
+                    <Col xs={3}>
+                      {c.description}
+                    </Col>
+                    <Col xs={3}>
+                      {c.make} / {c.model}
+                    </Col>
+                    <Col xs={2}>
+                      {c.km ? c.km + ' km' : '' }
+                    </Col>
+                    <Col xs={2}>
+                      {c.estimatedate}
+                    </Col>
+                  </Row>
+                  { c.inMaintenance &&
+                    <Row>
+                      <Col xs={4}>
+                        <Button onClick={(e) => handleNotMaintenanceClick(e, i)}>Remove from maintenance</Button>
+                      </Col>
+                    </Row>
+                  }
+                </Col>
+              </Row>
+            </ListGroup.Item>)
+        }
+      </ListGroup>
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Send to maintenance</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Row>
+            <Col xs={4}>Id</Col>
+            <Col xs={4}>
+              {editRecord.id}
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={4}>
+              <label for="person">Person</label>
+            </Col>
+            <Col xs={4}>
+              <input name="person" id="person" type="text" value={editRecord.person} onChange={handleChange}></input>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={4}>            
+              <label for="estimatedate">Estimate date</label>
+            </Col>
+            <Col xs={4}>            
+              <input name="estimatedate" id="estimatedate" type="text" value={editRecord.estimatedate} onChange={handleChange}></input>
+            </Col>
+          </Row>      
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleSaveClick}>Save changes</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+
+const isDev = process.env.NODE_ENV !== 'production';
+const PORT = process.env.PORT || 5000;
+
+
+// Multi-process to utilize all CPU cores.
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  });
+
+} else {
+  const app = express();
+
+  // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, '../exam/build')));
+
+  app.use(express.json()) // for parsing application/json
+  //app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+  // Answer API requests.
+  app.get('/api/cars', function (req, res) {
+    let json = '';
+    fs.readdirSync('./server/data/').forEach(file => {
+      const data = fs.readFileSync('./server/data/' + file  , 'utf8')
+      json += (json.length == 0 ? '[' : ',' ) + data;
+    });
+    json += ']';
+    console.log(json);
+    res.set('Content-Type', 'application/json');
+    res.send(json);
+  });
+
+  app.post('/api/cars', function (req, res) {
+    const car = `{
+      "person":"${req.body.person || ''}",
+      "description":"${req.body.description || ''}",
+      "make":"${req.body.make || ''}",
+      "model":"${req.body.model || ''}",
+      "estimatedate":"${req.body.estimatedate || ''}",
+      "id":${req.body.id || ''},
+      "image":"${req.body.image || ''}",
+      "inMaintenance":${req.body.inMaintenance || false}
+    }`;
+    //Save file to storage
+    fs.writeFile(`./server/data/${req.query.id}.json`, car, err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+    });
+
+    res.end();
+  });
+
+  app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../exam/build', 'index.html'));
+  });
+
+  app.listen(PORT, function () {
+    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+  });
+}
